1b80b7f25403ef66247015620603c2fe9acbebd0
weepwood <weepwood@outlook.com>
2026-04-04 13:12:29 +0800

    feat(types): 添加.vue文件的模块声明
    - 声明支持导入.vue文件作为Vue组件
    - 使用DefineComponent类型注解组件定义
    - 解决TypeScript对.vue文件的类型识别问题

--- frontend-fantastic-admin/src/types/shims.d.ts
+++ frontend-fantastic-admin/src/types/shims.d.ts
@@ -11,3 +11,9 @@ declare const __SYSTEM_INFO__: {
   }
   lastBuildTime: string
 }
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}

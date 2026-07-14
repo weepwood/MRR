3c6a2e6c913913f2eab5945437d188f3d3fa8456
weepwood <weepwood@outlook.com>
2026-07-14 18:21:45 +0800

    refactor(data-quality): remove automatic scheduling configuration

--- backend-repo/src/main/java/com/zjcxph/imgapi/config/DataQualityMonitoringConfig.java
+++
@@ -1,9 +0,0 @@
-package com.zjcxph.imgapi.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class DataQualityMonitoringConfig {
-}

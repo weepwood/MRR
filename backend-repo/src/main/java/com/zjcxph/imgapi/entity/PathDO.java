a597ef4fdee5b0a74bb2896be1a1461be499a677
weepwood <weepwood@outlook.com>
2026-07-21 18:59:17 +0800

    feat: 扩展图片来源元数据

--- backend-repo/src/main/java/com/zjcxph/imgapi/entity/PathDO.java
+++ backend-repo/src/main/java/com/zjcxph/imgapi/entity/PathDO.java
@@ -4,10 +4,17 @@ import lombok.Data;
 
 @Data
 public class PathDO {
+    private Integer scanId;
     private String folder;
     private String filename;
     private String brxh;
     private String bah;
+    private String sjh;
+    private String sourceType;
+    private String sourceNode;
+    private String sourceRef;
+    private String ossUrl;
+    private Long fileSize;
 
     public PathDO() {
     }
@@ -17,5 +24,29 @@ public class PathDO {
         this.filename = filename;
         this.brxh = brxh;
         this.bah = bah;
+    }
+
+    public PathDO(Integer scanId,
+                  String folder,
+                  String filename,
+                  String brxh,
+                  String bah,
+                  String sjh,
+                  String sourceType,
+                  String sourceNode,
+                  String sourceRef,
+                  String ossUrl,
+                  Long fileSize) {
+        this.scanId = scanId;
+        this.folder = folder;
+        this.filename = filename;
+        this.brxh = brxh;
+        this.bah = bah;
+        this.sjh = sjh;
+        this.sourceType = sourceType;
+        this.sourceNode = sourceNode;
+        this.sourceRef = sourceRef;
+        this.ossUrl = ossUrl;
+        this.fileSize = fileSize;
     }
 }

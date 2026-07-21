f8032f16b5653683eef57746ffa6b90fbd81b7ae
weepwood <weepwood@outlook.com>
2026-07-21 14:22:08 +0800

    test(export): cover backend PDF planning boundaries

--- backend-repo/src/test/java/com/zjcxph/imgapi/unit/controller/ArchiveExportControllerTest.java
+++ backend-repo/src/test/java/com/zjcxph/imgapi/unit/controller/ArchiveExportControllerTest.java
@@ -1,7 +1,9 @@
 package com.zjcxph.imgapi.unit.controller;
 
+import com.zjcxph.imgapi.common.Result;
 import com.zjcxph.imgapi.controller.ArchiveExportController;
 import com.zjcxph.imgapi.dto.req.BatchDownloadRequest;
+import com.zjcxph.imgapi.dto.resp.ArchiveExportPlanResponse;
 import com.zjcxph.imgapi.entity.PathDO;
 import com.zjcxph.imgapi.service.ArchiveExportService;
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -28,9 +31,42 @@ class ArchiveExportControllerTest {
     private ArchiveExportController controller;
 
     @Test
+    void plansSmallPartialPdfInBrowser() {
+        when(archiveExportService.prepareArchive("00789508", ""))
+                .thenReturn(export(3));
+
+        Result<ArchiveExportPlanResponse> result = controller.planPdf("789508", null, 2);
+
+        assertThat(result.getData().executionMode()).isEqualTo("CLIENT_PDF");
+        assertThat(result.getData().wholeArchive()).isFalse();
+    }
+
+    @Test
+    void plansWholeArchivePdfOnBackend() {
+        when(archiveExportService.prepareArchive("00789508", ""))
+                .thenReturn(export(3));
+
+        Result<ArchiveExportPlanResponse> result = controller.planPdf("789508", null, 3);
+
+        assertThat(result.getData().executionMode()).isEqualTo("BACKEND_STREAM");
+        assertThat(result.getData().wholeArchive()).isTrue();
+    }
+
+    @Test
+    void plansLargePartialPdfOnBackend() {
+        when(archiveExportService.prepareArchive("00789508", ""))
+                .thenReturn(export(30));
+
+        Result<ArchiveExportPlanResponse> result = controller.planPdf("789508", null, 21);
+
+        assertThat(result.getData().executionMode()).isEqualTo("BACKEND_STREAM");
+        assertThat(result.getData().wholeArchive()).isFalse();
+    }
+
+    @Test
     void streamsWholeArchiveZip() {
         when(archiveExportService.prepareArchive("00789508", ""))
-                .thenReturn(export());
+                .thenReturn(export(1));
 
         ResponseEntity<StreamingResponseBody> response = controller.downloadZip("789508", null);
 
@@ -41,7 +77,7 @@ class ArchiveExportControllerTest {
     @Test
     void streamsWholeArchivePdf() {
         when(archiveExportService.prepareArchive("00789508", ""))
-                .thenReturn(export());
+                .thenReturn(export(1));
 
         ResponseEntity<StreamingResponseBody> response = controller.downloadPdf("789508", null);
 
@@ -54,7 +90,7 @@ class ArchiveExportControllerTest {
     void streamsSelectedImagesAsPdf() {
         BatchDownloadRequest request = new BatchDownloadRequest();
         request.setIds(List.of("1", "2"));
-        when(archiveExportService.prepareBatch(List.of("1", "2"))).thenReturn(export());
+        when(archiveExportService.prepareBatch(List.of("1", "2"))).thenReturn(export(2));
 
         ResponseEntity<StreamingResponseBody> response = controller.downloadSelectedPdf(request);
 
@@ -62,9 +98,16 @@ class ArchiveExportControllerTest {
         assertThat(response.getHeaders().getContentType().toString()).isEqualTo("application/pdf");
     }
 
-    private ArchiveExportService.BatchZipExport export() {
-        return new ArchiveExportService.BatchZipExport(List.of(
-                new PathDO("25.03.15", "page.png", "605746", "00789508")
-        ));
+    private ArchiveExportService.BatchZipExport export(int count) {
+        return new ArchiveExportService.BatchZipExport(
+                IntStream.rangeClosed(1, count)
+                        .mapToObj(index -> new PathDO(
+                                "25.03.15",
+                                "page-" + index + ".png",
+                                "605746",
+                                "00789508"
+                        ))
+                        .toList()
+        );
     }
 }

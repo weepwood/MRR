247c6508bc7c7fe1800271d6d32159eecaf51165
weepwood <weepwood@outlook.com>
2026-07-22 23:25:53 +0800

    功能：增加 OSS 文件浏览菜单入口

--- frontend-fantastic-admin/src/router/routes.ts
+++ frontend-fantastic-admin/src/router/routes.ts
@@ -214,6 +214,11 @@ const asyncRoutes: Route.recordMainRaw[] = [
         component: () => import('@/views/oss-migration/index.vue'),
         meta: { title: 'OSS 迁移管理', icon: 'i-ant-design:cloud-upload-outlined', auth: ['record:read'], cache: true },
       },
+      {
+        path: '/oss-browser',
+        component: () => import('@/views/oss-browser/index.vue'),
+        meta: { title: 'OSS 文件浏览', icon: 'i-ant-design:folder-open-twotone', auth: ['record:read'], cache: true },
+      },
       archiveEmbeddedRoute,
     ],
   },
